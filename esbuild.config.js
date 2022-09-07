3d537ff6aa2cadd4f109e5c972a712303f5b1f75
杨波 <yangbo02@longfor.com>
2022-09-07 16:44:14 +0800

    修改esbuild配置

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,33 +3,43 @@ import progress from 'esbuild-plugin-progress'
 import { esbuildPluginFileSize } from 'esbuild-plugin-filesize'
 import moduleMaps from './modules.js'
 
-build({
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  format: 'esm',
-  outfile: 'dist/fe-utils.js',
-  treeShaking: true,
-  tsconfig: 'tsconfig.json',
-  plugins: [
-    progress(),
-    esbuildPluginFileSize({
-      theme: 'dark',
-      showPluginTitle: true,
-      showMinifiedSize: true,
-      showGzippedSize: true,
-      showBrotliSize: true
-    })
-  ]
-})
+;(async () => {
+  await build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    format: 'esm',
+    outfile: 'dist/index.esm.js',
+    treeShaking: true,
+    tsconfig: 'tsconfig.json',
+    plugins: [
+      progress(),
+      esbuildPluginFileSize()
+    ]
+  })
 
-build({
-  entryPoints: Object.values(moduleMaps),
-  bundle: true,
-  format: 'esm',
-  outdir: 'dist/modules',
-  treeShaking: true,
-  tsconfig: 'tsconfig.json',
-  plugins: [
-    progress()
-  ]
-})
+  await build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    format: 'cjs',
+    outfile: 'dist/index.cjs.js',
+    treeShaking: true,
+    tsconfig: 'tsconfig.json',
+    plugins: [
+      progress(),
+      esbuildPluginFileSize()
+    ]
+  })
+
+  await build({
+    entryPoints: Object.values(moduleMaps),
+    bundle: true,
+    format: 'esm',
+    outdir: 'dist/modules',
+    treeShaking: true,
+    tsconfig: 'tsconfig.json',
+    plugins: [
+      progress()
+    ]
+  })
+})()
+
